89a55c078afd06a33f9f68b8399cd1b0b2f30aa7
Kyle <kburton@relaynetwork.com>
2011-08-03 14:42:26 -0400

    init repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# perCEPtor
+
+Clojure bindings for [Esper](http://esper.codehaus.org/)
+
+# License
+
+Licensed under [EPL](http://www.eclipse.org/legal/epl-v10.html)
+
---
+++ test/perceptor/test/core.clj
@@ -0,0 +1,6 @@
+(ns perceptor.test.core
+  (:use [perceptor.core])
+  (:use [clojure.test]))
+
+#_(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
