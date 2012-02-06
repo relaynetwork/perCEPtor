7f8d9b8cda74d81be0aed7a6399db5bdbe58b850
Kyle Burton <kburton@relaynetwork.com>
2012-02-05 23:14:29 -0500

    drop snapshots

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
   :dependencies [[org.clojure/clojure                         "1.2.1"]
                  [org.clojure/clojure-contrib                 "1.2.0"]
                  [com.espertech/esper                         "4.3.0"]
-                 [org.clojars.kyleburton/clj-etl-utils        "1.0.38-SNAPSHOT"]
-                 [com.relaynetwork/clorine                    "1.0.6-SNAPSHOT"]])
+                 [org.clojars.kyleburton/clj-etl-utils        "1.0.38"]
+                 [com.relaynetwork/clorine                    "1.0.6"]])
