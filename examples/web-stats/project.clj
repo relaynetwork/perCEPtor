b76346d9528eba890da23f53f8a0079c614914f6
Paul and Kyle <kburton@relaynetwork.com>
2014-04-07 19:06:53 -0400

    update deps

--- examples/web-stats/project.clj
+++ examples/web-stats/project.clj
@@ -4,7 +4,9 @@
   :local-repo-classpath true
   :dev-resources-path "dev-resources"
   :main web-stats.main
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.relaynetwork/perceptor "1.0.0-SNAPSHOT"]
-                 [com.github.kyleburton/teporingo "1.0.1-SNAPSHOT"]
-                 [noir "1.1.0"]])
+  :dependencies [[org.clojure/clojure             "1.5.1"]
+                 [com.github.kyleburton/perceptor "1.0.0-SNAPSHOT"]
+                 [com.github.kyleburton/teporingo "2.1.27"]
+                 [org.clojure/data.json           "0.2.2"] ;; etl utils needs 0.2.2 if we're on clojure 1.2
+                 [hiccup                          "1.0.5"]
+                 [noir                            "1.3.0"]])
