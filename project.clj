c019fc5e160b2c87a5459ad15714345d3b6097c5
Paul and Kyle <kburton@relaynetwork.com>
2014-04-07 18:31:09 -0400

    lein-release plugin: preparing 1.0.0 release

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.github.kyleburton/perceptor "1.0.0-SNAPSHOT"
+(defproject com.github.kyleburton/perceptor "1.0.0"
   :description "Clojure bindings for Esper: http://esper.codehaus.org/"
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   :local-repo-classpath true
-  :dependencies [[org.clojure/clojure                         "1.3.0"]
-                 [com.espertech/esper                         "4.7.0"]
-                 [org.clojars.kyleburton/clj-etl-utils        "1.3.7"]
-                 [com.relaynetwork/clorine                    "1.0.6"]])
+  :dependencies [[org.clojure/clojure                         "1.5.1"]
+                 [com.espertech/esper                         "4.11.0"]
+                 [com.github.kyleburton/clj-etl-utils         "1.0.79"]
+                 [com.relaynetwork/clorine                    "1.0.17"]])
